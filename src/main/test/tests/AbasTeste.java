18d43eca5c268141d3df1d0ba0c40639a52ac135
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-09-01 09:22:10 -0300

    Add: Create test of the Abas

---
+++ src/main/test/tests/AbasTeste.java
@@ -0,0 +1,30 @@
+package tests;
+
+import core.BaseTests;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import pages.AbasPage;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class AbasTeste extends BaseTests {
+    private AbasPage page = new AbasPage();
+    @Before
+    public void setUpForm(){
+        page.clickByText("Abas");
+    }
+    @After
+    public void tearDownForm() {
+        createScreenShot();
+        driver.resetApp();
+    }
+    @Test
+    public void shouldInteractWithAbas(){
+        assertTrue(page.isAba1());
+
+        page.selectAba2();
+
+        assertTrue(page.isAba2());
+    }
+}
